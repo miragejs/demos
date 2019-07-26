36acc34c1b3636209c17ed948e6971463a2edbfc
Ryan Toronto <ryanto@gmail.com>
2019-07-25 22:25:36 -0400

    put mirage in index

--- react/src/App.js
+++ react/src/App.js
@@ -1,60 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { Server, Model, Factory, JSONAPISerializer } from '@miragejs/server';
 
-let server = new Server({
-  models: {
-    user: Model
-  },
-  factories: {
-    user: Factory.extend({
-      name(i) {
-        return `User ${i}`;
-      }
-    })
-  },
-  serializers: {
-    application: JSONAPISerializer
-  },
-  scenarios: {
-    default(server) {
-      server.createList('user', 10);
-    }
-  },
-  baseConfig() {
-    this.namespace = 'api';
-    this.get('/users');
-    this.passthrough();
-  }
-});
+export default function App() {
+  let [ users, setUsers ] = useState([]);
 
-window.server = server;
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: []
+  useEffect(() => {
+    let fetchUsers = async function() {
+      let response = await fetch('/api/users');
+      let json = await response.json();
+      setUsers(json.users);
     };
-  }
 
-  componentDidMount() {
-    fetch('/api/users')
-      .then(response => response.json())
-      .then(json => this.setState({ users: json.data }));
-  }
+    fetchUsers();
+  }, []);
 
-  render() {
-    return (
-      <ul>
-        {this.state.users.map(user =>
-          <li key={user.id}>
-            {user.attributes.name}
-          </li>
-        )}
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      {users.map(user =>
+        <li key={user.id}>
+          {user.name}
+        </li>
+      )}
+    </ul>
+  );
 }
-
-export default App;
--- react/src/index.js
+++ react/src/index.js
@@ -2,7 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Server, Model, Factory } from '@miragejs/server';
 import * as serviceWorker from './serviceWorker';
+
+let server = new Server({
+  models: {
+    user: Model
+  },
+  factories: {
+    user: Factory.extend({
+      name(i) {
+        return `User ${i}`;
+      }
+    })
+  },
+  scenarios: {
+    default(server) {
+      server.createList('user', 10);
+    }
+  },
+  baseConfig() {
+    this.namespace = 'api';
+    this.get('/users');
+    this.passthrough();
+  }
+});
+
+export { server };
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
